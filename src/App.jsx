b0b01691d46428b2edf2c6fccaae8ea47b4b501d
Vincent Bui <vincent.bui657@gmail.com>
2024-05-09 20:00:17 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {useState, useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  // Variable with your API Key
+  const apiKey = "98e3fb1f";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch(e) {
+      console.error(e)
+    }
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
+  // We pass movie as props to movie display
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
